724041ddd1ac2fec6d22667aa800d63ff1177c51
thieuhoang2002 <thieuviethoang7b@gmail.com>
2023-03-30 23:33:13 +0700

    hoang update

--- Source/assets/js/header.js
+++ Source/assets/js/header.js
@@ -4,7 +4,7 @@ function createHeader() {
     <div id="logo-header"></div>
     <div id="menu-header">
         <img class="line-header" src=".//assets/img/Line.png" style="order: 0;" alt="">
-        <p class="child-menu-header" style="width: 88px;order: 1;">Trang chủ</p>
+        <p class="child-menu-header" style="width: 88px;order: 1;"><a style="color: white; text-decoration: none;" href=".//index.html">Trang chủ</a></p>
         <img class="line-header" src=".//assets/img/Line.png" style="order: 2;" alt="">
         <p class="child-menu-header" style="width: 89px;order: 3;">Sản phẩm</p>
         <img class="line-header" src=".//assets/img/Line.png" style="order: 4;" alt="">
@@ -19,7 +19,7 @@ function createHeader() {
         <img class="line-header" src=".//assets/img/Line.png" style="order: 3;" alt="">
         <img class="icon-header" src=".//assets/img/add_shopping_cart.png" style="order: 4;" alt="">
         <img class="line-header" src=".//assets/img/Line.png" style="order: 5;" alt="">
-        <p id="login-signup" style="order: 6;"> <a href="#">Đăng nhập</a></p>
+        <p id="login-signup" style="order: 6;"> <a href=".//login.html">Đăng nhập</a></p>
     </div>
         `;
     return header;
