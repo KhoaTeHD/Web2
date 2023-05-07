acbe67cab2316c3d2c7a875919b5c9402a5f61ea
thieuhoang2002 <thieuviethoang7b@gmail.com>
2023-05-07 18:50:55 +0700

    update donhang

--- vutrudongho/my_order.php
+++ vutrudongho/my_order.php
@@ -20,10 +20,9 @@ if (!$conn) {
 $limit = 3;
 
 // truy vấn cơ sở dữ liệu để lấy số lượng tổng mục
-$query = sprintf("SELECT COUNT(*)FROM user;");
-$result = mysqli_query($conn, $query);
-$row = mysqli_fetch_array($result);
-$total_items = $row[0];
+$sql = sprintf("SELECT * FROM `order`");
+$result = mysqli_query($conn, $sql);
+$total_items = mysqli_num_rows($result);
 
 // tính toán tổng số trang
 $total_pages = ceil($total_items / $limit);
@@ -35,22 +34,9 @@ $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
 $start = ($current_page - 1) * $limit;
 
 // truy vấn cơ sở dữ liệu để lấy dữ liệu
-$query = sprintf("SELECT * FROM user LIMIT $start, $limit;");
+$query = sprintf("select * from `order` ORDER BY OderDate DESC limit $start, $limit;");
 $result = mysqli_query($conn, $query);
 
-// hiển thị dữ liệu
-while ($row = mysqli_fetch_array($result)) {
-  $userID = $row['UserID'];
-  $fullName = $row['FullName'];
-  $numberPhone = $row['NumberPhone'];
-  $email = $row['Email'];
-  $passwordUser = $row['Password'];
-  $houseRoadAddress = $row['HouseRoadAddress'];
-  $ward = $row['Ward'];
-  $district = $row['District'];
-  $province = $row['Province'];
-  $status = $row['Status'];
-}
 ?>
 
 <!DOCTYPE html>
@@ -96,68 +82,120 @@ while ($row = mysqli_fetch_array($result)) {
 
       </div>
       <div id="content-user">
-        <p style="margin-bottom: 4px;margin-top: 4px;margin-left: 4px;">Đơn hàng đã mua</p>
-        <div class="component_order"
-          style="display: flex;flex-direction: column;width: 100%;height: 24%;align-items: center;background-color: #fff;margin-bottom: 8px;">
+        <p style="margin-bottom: 16px;margin-top: 4px;margin-left: 4px;">Tổng số đơn hàng (<?php echo $total_items;?>)</p>
+
+        <?php
+        // hiển thị dữ liệu
+        while ($row = mysqli_fetch_array($result)) {
+          $orderID = $row['OrderID'];
+          $orderDate = $row['OderDate'];
+          $shippingFee = $row['ShippingFee'];
+          $orderDiscount = $row['OrderDiscount'];
+          $orderTotal = $row['OrderTotal'];
+          $address = $row['Address'];
+          $paymentID = $row['PaymentID'];
+          $phuongthucThanhToan = "";
+          if($paymentID == "PA01"){
+            $phuongthucThanhToan = "Thanh toán khi nhận hàng";
+          }
+          else{
+            $phuongthucThanhToan = "Internet Banking";
+          }
+          $voucherID = $row['VoucherID'];
+          $orderStatus = $row['OrderStatus'];
+          $trangThaiDonHang = "";
+          if($orderStatus =="S01"){
+            $trangThaiDonHang = "Chưa xác nhận";
+          }
+          elseif($orderStatus == "S02"){
+            $trangThaiDonHang = "Đã xác nhận";
+          }
+          elseif($orderStatus == "S03"){
+            $trangThaiDonHang = "Đang giao hàng";
+          }
+          else{
+            $trangThaiDonHang = "Đã giao hàng";
+          }
+          $userID = $row['UserID'];
+
+          echo '
+          <div class="component_order"
+          style="display: flex;flex-direction: column;width: 100%;height: fit-content;align-items: center;background-color: #fff;margin-bottom: 8px;padding-bottom: 8px;">
           <div class="header-component-order"
-            style="display: inline-flex;padding: 8px 8px 8px 8px;align-items: left;width: 100%;">
-            <p style="display:block;text-align: right;">Đã giao hàng</p>
+            style="display: inline-flex;padding: 8px 8px 8px 8px;align-items: left;width: 100%;position:relative;">
+            <a style="display:block;text-align: right;" href="">Xem chi tiết</a>
+            <p style="display:block;position: absolute;right:8px;">Mã đơn hàng: '.$orderID.'</p>
           </div>
           <hr style="width: 100%;">
+          <div class="main-component-order-title"
+            style="display:flex;flex-direction: row;justify-content: space-between;align-items: center;width: 95%;margin-top: 4px;margin-bottom: 10px;">
+            <p style="width: 20%;">Thời gian</p>
+            <p style="width:14%;">Phí vận chuyển</p>
+            <p style="width:14%;">Giảm giá</p> 
+            <p style="width:14%;">Tổng tiền</p>
+            <p style="width:24%;">Hình thức thanh toán</p>
+            <p style="width: 14%;">Trạng thái</p>
+          </div>
           <div class="main-component-order"
             style="display:flex;flex-direction: row;justify-content: space-between;align-items: center;width: 95%;margin-top: 4px;">
-            <img src=".//assets/Img/productImg/0001670_white_550.png" alt="" width="75">
-            <p style="width: 40%;">Seiko 5 Field Sports Style SRPG29K1 – Nam – Automatic (Tự Động) – Mặt Số 39.4mm,
-              Chống Nước 10ATM, Bộ Máy In-House</p>
-            <p>170000 VNĐ</p>
-            <p>Số lượng: 1</p>
+            <p style="width: 20%;">'.$orderDate.'</p>
+            <p style="width: 14%;">'.$shippingFee.' đ</p>
+            <p style="width: 14%;">'.$orderDiscount.' đ</p> 
+            <p style="width: 14%;">'. $orderTotal.' đ</p>
+            <p style="width: 24%;">'.$phuongthucThanhToan.'</p>
+            <p style="width: 14%;">'.$trangThaiDonHang.'</p>
+          </div>
+        </div>
+          
+          ';
+        }
+        ?>
+        <!-- <div class="component_order"
+          style="display: flex;flex-direction: column;width: 100%;height: fit-content;align-items: center;background-color: #fff;margin-bottom: 8px;padding-bottom: 8px;">
+          <div class="header-component-order"
+            style="display: inline-flex;padding: 8px 8px 8px 8px;align-items: left;width: 100%;position:relative;">
+            <a style="display:block;text-align: right;" href="">Xem chi tiết</a>
+            <p style="display:block;position: absolute;right:8px;">Mã đơn hàng: OD00000001</p>
+          </div>
+          <hr style="width: 100%;">
+          <div class="main-component-order-title"
+            style="display:flex;flex-direction: row;justify-content: space-between;align-items: center;width: 95%;margin-top: 4px;margin-bottom: 10px;">
+            <p style="width: 18%;">Thời gian</p>
+            <p style="width: 10%;">Số lượng</p>
+            <p style="width:14%;">Phí vận chuyển</p>
+            <p style="width:12%;">Giảm giá</p> 
+            <p style="width:12%;">Tổng tiền</p>
+            <p style="width:22%;">Hình thức thanh toán</p>
+            <p style="width: 12%;">Trạng thái</p>
+          </div>
+          <div class="main-component-order"
+            style="display:flex;flex-direction: row;justify-content: space-between;align-items: center;width: 95%;margin-top: 4px;">
+            <p style="width: 18%;">2023-05-01 20:13:19</p>
+            <p style="width: 10%;">1</p>
+            <p style="width: 14%;">15000 đ</p>
+            <p style="width: 12%;">50000 đ</p> 
+            <p style="width: 12%;">7105000 đ</p>
+            <p style="width: 22%;">Thanh toán khi nhận hàng</p>
+            <p style="width: 12%;">Đã giao hàng</p>
+          </div>
+        </div> -->
+
+
+        <div
+          style="width: 100%;height: 26px;position: absolute;bottom: 0;margin-bottom: 60px;display: flex;flex-direction:row;align-items: center;justify-content: center;">
+          <div id="numberPhanTrang"
+            style="width: fit-content;display: flex;flex-direction:row;align-items: center;justify-content: center;position: fixed;z-index: 999999;">
+            <p style="padding: 4px;">
+              <?php
+              // hiển thị các liên kết phân trang
+              for ($page = 1; $page <= $total_pages; $page++) {
+                echo '<a class="number-ptrang-' . $page . '" onclick="changeColor()" style="text-decoration: none;padding: 4px 8px;background-color:#ccc;" href="?page=' . $page . '">' . $page . '</a> ';
+              }
+              ?>
+            </p>
           </div>
         </div>
 
-        <div class="component_order"
-          style="display: flex;flex-direction: column;width: 100%;height: 24%;align-items: center;background-color: #fff;margin-bottom: 8px;">
-          <div class="header-component-order"
-            style="display: inline-flex;padding: 8px 8px 8px 8px;align-items: left;width: 100%;">
-            <p style="display:block;text-align: right;">Đã giao hàng</p>
-          </div>
-          <hr style="width: 100%;">
-          <div class="main-component-order"
-            style="display:flex;flex-direction: row;justify-content: space-between;align-items: center;width: 95%;margin-top: 4px;">
-            <img src=".//assets/Img/productImg/0001670_white_550.png" alt="" width="75">
-            <p style="width: 40%;">Seiko 5 Field Sports Style SRPG29K1 – Nam – Automatic (Tự Động) – Mặt Số 39.4mm,
-              Chống Nước 10ATM, Bộ Máy In-House</p>
-            <p>170000 VNĐ</p>
-            <p>Số lượng: 1</p>
-          </div>
-        </div>
-
-        <div class="component_order"
-          style="display: flex;flex-direction: column;width: 100%;height: 24%;align-items: center;background-color: #fff;">
-          <div class="header-component-order"
-            style="display: inline-flex;padding: 8px 8px 8px 8px;align-items: left;width: 100%;">
-            <p style="display:block;text-align: right;">Đã giao hàng</p>
-          </div>
-          <hr style="width: 100%;">
-          <div class="main-component-order"
-            style="display:flex;flex-direction: row;justify-content: space-between;align-items: center;width: 95%;margin-top: 4px;">
-            <img src=".//assets/Img/productImg/0001670_white_550.png" alt="" width="75">
-            <p style="width: 40%;">Seiko 5 Field Sports Style SRPG29K1 – Nam – Automatic (Tự Động) – Mặt Số 39.4mm,
-              Chống Nước 10ATM, Bộ Máy In-House</p>
-            <p>170000 VNĐ</p>
-            <p>Số lượng: 1</p>
-          </div>
-        </div>
-        <div id="numberPhanTrang"
-          style="width: 100%;display: flex;flex-direction:row;align-items: center;justify-content: center;margin-top: 8px;">
-          <p style="padding: 4px;">
-            <?php
-            // hiển thị các liên kết phân trang
-            for ($page = 1; $page <= $total_pages; $page++) {
-              echo '<a class="number-ptrang-'.$page.'" onclick="changeColor()" style="text-decoration: none;padding: 4px 8px;background-color:#ccc;" href="?page=' . $page . '">' . $page . '</a> ';
-            }
-            ?>
-          </p>
-        </div>
 
       </div>
     </div>
@@ -170,17 +208,17 @@ while ($row = mysqli_fetch_array($result)) {
   </div>
   <!--End: Footer-->
   <script>
-function changeColor() {
-  var currentPage = parseInt(new URLSearchParams(window.location.search).get('page')); // Lấy trang hiện tại từ tham số truy vấn '?page'
+    function changeColor() {
+      var currentPage = parseInt(new URLSearchParams(window.location.search).get('page')); // Lấy trang hiện tại từ tham số truy vấn '?page'
 
-  if (currentPage) {
-    var currentElement = document.querySelector('.number-ptrang-' + currentPage); // Chọn phần tử tương ứng với trang hiện tại
+      if (currentPage) {
+        var currentElement = document.querySelector('.number-ptrang-' + currentPage); // Chọn phần tử tương ứng với trang hiện tại
 
-    if (currentElement) {
-      currentElement.style.backgroundColor = "orange"; // Thay đổi màu nền của phần tử thành màu cam
+        if (currentElement) {
+          currentElement.style.backgroundColor = "orange"; // Thay đổi màu nền của phần tử thành màu cam
+        }
+      }
     }
-  }
-}
 
   </script>
 </body>
